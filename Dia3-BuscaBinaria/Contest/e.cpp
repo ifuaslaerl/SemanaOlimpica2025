25cd2f4d3e6d35a4d3fad4bcac46a1f0a064fdd1
tucoramos <Tuturamos17@gmail.com>
2025-12-12 17:56:00 -0300

    Update e.cpp

--- Dia3-BuscaBinaria/Contest/e.cpp
+++ Dia3-BuscaBinaria/Contest/e.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long int ll;
 
-int main(){
+double buscaBinaria(double c, double f, double C){
+    int cont = 301;
+    while(cont--){
+        double m = c + (f - c)/2;
+        double x = m*m + sqrt(m);
+        if(C > x){
+            c = m;
+        }
+        if(C < x){
+            f = m;
+        }
+    }
+    return c;
+}
+
+int main() {
+    double C, x, resposta;
+    
+    cin >> C;
+
+    resposta = buscaBinaria(1, 1e5, C);
+    
+    cout << fixed << setprecision(10) << resposta << endl;
+
     return 0;
 }
